d275eb9bdd6e3d753a7b9cb4ed96e7c944c8aad3
Hizqeelrz <hizqeel1214@gmail.com>
2018-06-29 20:07:46 +0100

    Movie MVC added

---
+++ Vidle/Controllers/MoviesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidle.Models;
+
+namespace Vidle.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie()
+            {
+                Name = "Sherk"
+            };
+            return View(movie);
+        }
+    }
+}
