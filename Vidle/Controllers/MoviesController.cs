0c671edebc7daa8d848e4dddc9738091553d0a61
Hizqeelrz <hizqeel1214@gmail.com>
2018-06-30 19:59:33 +0100

    Movies Edit/New/Update actions updated fully

--- Vidle/Controllers/MoviesController.cs
+++ Vidle/Controllers/MoviesController.cs
@@ -65,5 +65,57 @@ namespace Vidle.Controllers
 
             return View(viewModel);
         }
+
+        public ActionResult New()
+        {
+            var genre = _context.Genres.ToList();
+
+            var viewModel = new MovieFormViewModel
+            {
+                Genres = genre
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+
+
+            var viewModel = new MovieFormViewModel
+            {
+                Movie = movie,
+                Genres = _context.Genres.ToList()
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
+        public ActionResult Save(Movie movie)
+        {
+            if (movie.Id == 0)
+            {
+                _context.Movies.Add(movie);
+            }
+            else
+            {
+                var movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
+
+                movieInDb.Name = movie.Name;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+                movieInDb.NumberInStock = movie.NumberInStock;
+                movieInDb.GenreId = movie.GenreId;
+            }
+
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Movies");
+
+        }
     }
 }
--- Vidle/Models/Movie.cs
+++ Vidle/Models/Movie.cs
@@ -14,11 +14,16 @@ namespace Vidle.Models
         [StringLength(255)]
         public string Name { get; set; }
 
+        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
         public DateTime DateAdded { get; set; }
+
+        [Display(Name = "Number In Stock")]
         public byte NumberInStock { get; set; }
 
         public Genre Genre { get; set; }
+
+        [Display(Name = "Genre")]
         public byte GenreId { get; set; }
 
     }
